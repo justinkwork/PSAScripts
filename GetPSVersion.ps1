b784e5627cca99d60d1e509c3b4470536ef0fdca
Justin Workman <justin.workman@cireson.com>
2022-10-17 13:42:14 -0500

    added label in output

--- GetPSVersion.ps1
+++ GetPSVersion.ps1
@@ -1,6 +1,7 @@
 $versions = $PSVersionTable.GetEnumerator()
 foreach ($v in $versions) {
-    if ($v.Name -eq 'PSVersion') {Write-Output $($v.Name + ": " + $v.value.tostring())}
+    if ($v.Name -eq 'PSVersion') { Write-Output $($v.Name + ": " + $v.value.tostring()) }
 }
+Write-Output "PS Versions:`n"
 write-output (get-host).version.toString()
 write-output $host.version.toString()
