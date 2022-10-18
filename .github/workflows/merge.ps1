52c6358e6e3bf87ef09d4e83b9f7f6e623a13e87
Justin Workman <justin.workman@cireson.com>
2022-10-17 22:23:48 -0500

    testing pwd in ps

--- .github/workflows/merge.ps1
+++ .github/workflows/merge.ps1
@@ -1,5 +1,18 @@
 write-output "Testing merge script"
 
-foreach ($file in ($env:filesOutput | convertfrom-json)) {
-    $file
-}
+# $psaScriptClass = get-scsmclass -name Cireson.Powershell.Activity.Script$ -computerName $env:ServerName
+# foreach ($file in ($env:filesOutput | convertfrom-json)) {
+#     $fileName = $file.split('/')
+#     if ($filename[0] -ne '.github') {
+#         $shortName = $fileName[$fileName.length - 1]
+#         $fnParts = $shortName.split('.')
+#         if ($fnParts[$fnParts.length - 1] -eq '.ps1') {
+#             $existing = get-scsmobject -class $psaScriptClass -computerName $env:ServerName
+#             if ($existing) {
+#                 $scriptBody = 
+#                 set-scsmobject -property PowerShellScript
+#             }
+#         }
+#     }
+# }
+$pwd
