2a60ce1c2c20b94f367669ec921f1b090d0d6e3e
Justin Workman <justin.workman@cireson.com>
2022-10-17 23:08:33 -0500

    removed webhook url

--- Send-TeamsWelcome.ps1
+++ Send-TeamsWelcome.ps1
@@ -2,7 +2,7 @@
     $srId
 )
 Import-Module Smlets
-$webhookURL = "https://jkwcireson.webhook.office.com/webhookb2/8d0881af-7dad-4405-80e5-4505cee10216@5fa9364d-a051-4756-8589-8de9e3376718/IncomingWebhook/2b7229577afe4f6793b9fda1c1aa79e7/c0c950fb-f3bd-49a9-9f7d-1ea191b22533"
+$webhookURL = ""
 
 $thisSR = get-scsmobject -id $srId
 $inputJSON = $thisSR.inputJson | ConvertFrom-Json
