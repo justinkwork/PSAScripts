c8fed388ac134b1ebcd5e38a52b938073cf48c5f
Justin Workman <justin.workman@cireson.com>
2022-10-28 10:28:31 -0500

    added file logging

--- Get-TikitUser.ps1
+++ Get-TikitUser.ps1
@@ -1,5 +1,33 @@
 param($srId)
 Import-Module SMLets
+
+function Add-LogMessage {
+    param($Message, $Severity, $Path)
+    if (!$Severity) {
+        $Severity = 3
+    }
+    if (!$path) {
+        $Path = $env:temp
+    }
+   
+    $severities = @(
+        [pscustomobject]@{
+            Ordinal = 1
+            Type = "ERROR"
+        },
+        [pscustomobject]@{
+            Ordinal = 2
+            Type = "WARN"
+        },
+        [pscustomobject]@{
+            Ordinal = 3
+            Type = "INFO"
+        }
+    )
+    $outMessage = "$((get-date -Format "y-MM-d HH:mm:ss")) - $($severities | ?{$_.Ordinal -eq $Severity} | select -ExpandProperty Type) - $Message"
+    $outMessage | Out-File $path -Encoding ascii -Append
+
+}
 function Get-TikitUser {
     param(
         $username, $tikitToken
@@ -12,6 +40,10 @@ function Get-TikitUser {
     return $user.value[0]
 }
 
+$logPath = "c:\Logs"
+
+Add-LogMessage -Message $srId -path $logpath
+
 $psaVariables = Get-Content "c:\PSAVariables.txt" | ConvertFrom-Json
 
 $token = $psaVariables."Get-TikitUser".tikitToken
@@ -21,7 +53,7 @@ $SrRels = get-scsmrelationshipobject -bysource $thisSR
 $affecterUserRel = $srRels | ?{$_.RelationshipId -eq 'dff9be66-38b0-b6d6-6144-a412a3ebd4ce'}
 $user = get-scsmobject -Id $affecterUserRel.targetobject.Id
 $tikitUser = Get-TikitUser -username $user.UserName -tikitToken $token
-
+Add-LogMessage -Message "Affected User: $($user.DisplayName)" -path $logpath
 $activityRels = $SrRels | ?{$_.RelationshipId -eq "2da498be-0485-b2b2-d520-6ebd1698e61b"}
 $activities = @()
 foreach ($a in $activityRels) {
@@ -29,11 +61,14 @@ foreach ($a in $activityRels) {
     $activities += $emoAct
 }
 $thisActivity = $activities|?{$_.description -eq 'Get-TikitUser'}
+Add-LogMessage -Message "This Activity: $($thisActivity.Name)" -path $logpath
 $nextActivity = $activities | ?{$_.sequenceId -eq $($thisActivity.SequenceId + 1)}
 
 if ($nextActivity) {
+    Add-LogMessage -Message "Next Activity: $($nextActivity.Name)" -path $logpath
     $nextActivity.Text6 = $tikitUser.Id
 }
 else {
     write-output "Could not get next activity!"
+    Add-LogMessage -Message "Could not get next activity!" -path $logpath
 }
